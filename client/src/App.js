44e49f288534a389db704d7f95d93aa45c43ea14
DavTheDev20 <reiddavin@outlook.com>
2021-12-28 12:23:27 -0500

    Attempt to add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Create from './pages/Create';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <h1>Hello World</h1>
+      <h1>Welcome to the Blog App.</h1>
     </div>
   );
 };
