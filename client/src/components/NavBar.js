378f72f8d9ed7ddf451b14a1b0cdf8cf178b2a1a
DavTheDev20 <reiddavin@outlook.com>
2021-12-28 11:05:02 -0500

    Add functionality to hamburger icon with the creation of a mobile menu

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,35 +1,52 @@
+import { useState } from 'react';
+
 const NavBar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <nav>
-      <h3>Blog App</h3>
-      <ul className="standard-menu">
-        <li>
-          <a href="#">Link</a>
-        </li>
-        <li>
-          <a href="#">Link</a>
-        </li>
-        <li>
-          <a href="#">Link</a>
-        </li>
-      </ul>
-      <button className="hamburger-icon" onClick={() => console.log('Clicked')}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </button>
-      <ul className="hamburger-menu">
-        <li>
-          <a href="#">Link</a>
-        </li>
-        <li>
-          <a href="#">Link</a>
-        </li>
-        <li>
-          <a href="#">Link</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <h3>Blog App</h3>
+        <ul className="standard-menu">
+          <li>
+            <a href="#">Link</a>
+          </li>
+          <li>
+            <a href="#">Link</a>
+          </li>
+          <li>
+            <a href="#">Link</a>
+          </li>
+        </ul>
+        <button
+          className="hamburger-icon"
+          onClick={() =>
+            setShowMobileMenu((prevValue) =>
+              prevValue === true ? false : true
+            )
+          }
+        >
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </button>
+      </nav>
+      <div
+        className="mobile-menu"
+        style={showMobileMenu ? { display: 'block' } : { display: 'none' }}
+      >
+        <ul>
+          <li>
+            <a href="#">Link</a>
+          </li>
+          <li>
+            <a href="#">Link</a>
+          </li>
+          <li>
+            <a href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
